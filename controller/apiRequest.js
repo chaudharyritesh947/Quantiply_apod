f369025b207e5af565543f33e70fc38f81adfe0d
ritesh <chaudharyritesh947@gmail.com>
2021-11-14 01:20:27 +0530

    Implemented the request module

--- controller/apiRequest.js
+++ controller/apiRequest.js
@@ -1,19 +1,12 @@
-const https = require('https');
-const todayDate = new Date().toISOString().slice(0, 10);
-
+const request = require('request');
+ 
 const callNasaAPODApi = (query, callback) => {
     let url = `https://api.nasa.gov/planetary/apod?api_key=PXqBrUGI372XPO3keCFbaM0g3FOA6exxNaeX1YHN&date=`+`${query.date}`;
-    https.get(url, (res) =>{
-        let data = '';
-        res.on('data', (chunk)=>{
-            data +=chunk;
-        });
-
-        res.on('end' , ()=>{
-            return callback(data)
-        });
-    }).on('error',(err)=>{
-        console.log("error"+err.message)
+    request(url, { json: true }, (err, res, body) => {
+    if (err) { 
+        return callback(err);
+     }
+    return callback(body);
     });
 }
 
